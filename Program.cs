87f877bd53a7a74285ce303987f89b5ea197083c
Rajesh <padigarajesh219@gmail.com>
2021-11-12 11:22:07 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
